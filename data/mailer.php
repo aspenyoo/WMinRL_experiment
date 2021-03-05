ac897e81fadfcba16604e96ab8d671f99515a7be
arzou <amyzou@berkeley.edu>
2021-03-05 15:47:50 -0800

    updated saving functionality

---
+++ data/mailer.php
@@ -0,0 +1,54 @@
+<?php
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+require '../../PHPMailer/src/Exception.php';
+require '../../PHPMailer/src/PHPMailer.php';
+require '../../PHPMailer/src/SMTP.php';
+
+if (isset($_POST['file_name']) == true)
+{
+    $file_name = $_POST['file_name']; // mturk_id
+}
+
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'ssl://smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = '';                     // SMTP username
+    $mail->Password   = '';                               // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom('', 'Server Mailer');
+    // $mail->addAddress('', 'Recipient');     // Add a recipient
+   $mail->addAddress('');               // Name is optional
+    // $mail->addCC('cc@example.com');
+//    $mail->addBCC('bcc@example.com');
+
+    // Attachments
+    $mail->addAttachment($file_name);         // Add attachments
+//    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'WMinRL Data';
+    $mail->Body    = "Subject's CSV Data is attached.";
+    $mail->AltBody = "Subject's CSV Data is attached.";
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+?>
